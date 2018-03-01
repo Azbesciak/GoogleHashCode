f4f00757fee19c1ce41b51582859759a20d755c5
Witold Kupś <witold.kups@interlan.pl>
2018-03-01 19:14:06 +0100

    First line parse

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,23 +1,33 @@
 import java.awt.*;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        String[] firstRow = s.split("\\s");
-        firstRow[0]
+
 
     }
 
-    private int parse(String s) {
-        return Integer.valueOf(s);
+    private Task createTask() {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] firstRow = s.split("\\s");
+        int rows = parseInt(firstRow[0]);
+        int columns = parseInt(firstRow[1]);
+        int vehicles = parseInt(firstRow[2]);
+        int rides = parseInt(firstRow[3]);
+        int bonus = parseInt(firstRow[4]);
+        int steps = parseInt(firstRow[5]);
+
+
     }
 
 
     class Task {
-        private int[][] Vehicle;
-
+        List<Vehicle> vehicles;
+        List<Route> routes;
     }
 
     class Vehicle {
