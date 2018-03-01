0d6bef9b64de689c20bb3cda5b6bd3443e6182c0
Jakub Tomczak <jakub.tomczak@live.com>
2018-03-01 20:06:01 +0100

    gfdgd

--- src/main/java/RideFinder.java
+++ src/main/java/RideFinder.java
@@ -1,8 +1,29 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RideFinder {
-    public static Ride findNearestRide(List<Vehicle> vehicles, List<Ride> rides)
+    public Ride findNearestRide(List<Vehicle> vehicles, List<Ride> rides)
     {
-        return null;
+        List<Vehicle> nonInRide = vehicles.stream().filter(x -> !x.isInRide).collect(Collectors.toList());
+        Integer shortestDistance = Integer.MAX_VALUE;
+        Vehicle nearestVehicle = null;
+        for(Vehicle vehicle : nonInRide )
+        {
+            for(Ride ride : rides)
+            {
+                Integer distance = getDistance(vehicle, ride);
+                if(distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    nearestVehicle = vehicle;
+                }
+            }
+        }
+        return
+    }
+
+    public Integer getDistance(Vehicle vehicle, Ride ride)
+    {
+        return Math.abs(vehicle.currentPosition.x - ride.destination.x) + Math.abs(vehicle.currentPosition.y - ride.destination.y);
     }
 }
