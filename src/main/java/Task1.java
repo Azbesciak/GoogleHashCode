7ded27cf7d485c4d5f5aa0a3b19911b0b021c83f
Witold Kupś <witold.kups@interlan.pl>
2018-03-01 19:07:49 +0100

    initial struct

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,5 +1,33 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] firstRow = s.split("\\s");
+        firstRow[0]
 
+    }
+
+    private int parse(String s) {
+        return Integer.valueOf(s);
+    }
+
+
+    class Task {
+        private int[][] Vehicle;
+
+    }
+
+    class Vehicle {
+
+    }
+
+    class Route {
+        Point source;
+        Point destination;
+        int earliestStart;
+        int latestFinist;
     }
 }
